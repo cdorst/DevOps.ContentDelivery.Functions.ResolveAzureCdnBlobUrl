90f53ddf63cec1ccb93fbcf1d21688defbf4a863
Christopher Dorst <chris@dorst.email>
2017-12-30 22:31:12 -0600

    Add UrlResolver

---
+++ DevOps.ContentDelivery.Functions.ResolveAzureCdnBlobUrl/UrlResolver.cs
@@ -0,0 +1,12 @@
+﻿using DevOps.ContentDelivery.Structures.AzureBlob;
+using System;
+using static DevOps.ContentDelivery.Functions.ResolveAzureCdnContentUrl.UrlResolver;
+
+namespace DevOps.ContentDelivery.Functions.ResolveAzureCdnBlobUrl
+{
+    public static class UrlResolver
+    {
+        public static Uri Uri(string endpointName, Blob blob)
+            => Url(endpointName, $"{blob.Container}/{blob.Name}");
+    }
+}
